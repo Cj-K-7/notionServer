e2060705be376a7be6864bd05c5eebc5a4a100f9
Cj-K-7 <kllt7059@gmail.com>
2022-10-15 19:24:21 +0900

    webGL init + tutorial

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect } from "react";
+import Canvas from "./Components/Global/Canvas";
 
 function App() {
   useEffect(() => {
     fetch("http://localhost:5000/api/listAll");
   }, []);
-  return <div></div>;
+  return <Canvas />;
 }
 
 export default App;
---
+++ frontend/src/Components/Global/Canvas.tsx
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+import "./Canvas.css";
+import WebGL from "./WebGl";
+
+interface ICanvas extends React.CanvasHTMLAttributes<HTMLCanvasElement> {}
+
+const Canvas: React.FC<ICanvas> = (props) => {
+  useEffect(() => {
+    WebGL.init().then(({ canvas, gl }) => WebGL.start(canvas, gl));
+  }, []);
+  return (
+    <div>
+      <canvas id="glCanvas" {...props}>
+        Your browser doesn't appear to support the HTML5{" "}
+        <code>&lt;canvas&gt;</code> element.
+      </canvas>
+    </div>
+  );
+};
+
+export default Canvas;
