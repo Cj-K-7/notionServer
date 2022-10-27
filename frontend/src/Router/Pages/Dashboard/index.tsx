d4bfc671526e45ac2226efaf56fffd6dcf88dc55
Cj-K-7 <kllt7059@gmail.com>
2022-10-27 14:09:39 +0900

    [FRONT]locating to canvas when idle

--- frontend/src/Router/Pages/Dashboard/index.tsx
+++ frontend/src/Router/Pages/Dashboard/index.tsx
@@ -1,4 +1,42 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 const Dashboard = () => {
+  //Hooks
+  const navigate = useNavigate();
+
+  //functions
+
+  //Set onMount 'idle' base behaviors
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+    const timeOutCallback = () => {
+      navigate("/canvas");
+    };
+    const timeReseter = () => {
+      console.log("hello");
+      clearTimeout(timer);
+      timer = setTimeout(timeOutCallback, 7000);
+    };
+    //Init timer on window loaded
+    window.onload = timeReseter;
+
+    //Reset Every interactions by EventListener
+    document.addEventListener("click", timeReseter);
+    document.addEventListener("mousemove", timeReseter);
+    document.addEventListener("touchstart", timeReseter);
+    document.addEventListener("touchend", timeReseter);
+    document.addEventListener("keydown", timeReseter);
+
+    return () => {
+      document.removeEventListener("click", timeReseter);
+      document.removeEventListener("mousemove", timeReseter);
+      document.removeEventListener("touchstart", timeReseter);
+      document.removeEventListener("touchend", timeReseter);
+      document.removeEventListener("keydown", timeReseter);
+    };
+  }, []);
+
   return <div>Dashboard</div>;
 };
 
