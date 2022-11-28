ae15816b873375110bc930d963be4e5df3ebb434
Cj-K-7 <kllt7059@gmail.com>
2022-11-28 23:01:38 +0900

    [FRONTEND] XML request CLASS part 1

--- frontend/src/APIs/base.ts
+++ frontend/src/APIs/base.ts
@@ -1,5 +1,45 @@
-const baseURL = window.location.origin;
+class Request {
+  //Base Properties
+  baseURL: string = window.location.origin;
+  request: XMLHttpRequest = new XMLHttpRequest();
+  onprogress = (event: ProgressEvent<EventTarget>) => {};
+  onload = (event: ProgressEvent<EventTarget>) => {};
+  onerror = (event: ProgressEvent<EventTarget>) => {};
+  onabort = (event: ProgressEvent<EventTarget>) => {};
 
-const request = new XMLHttpRequest();
+  //Constructor
+  constructor(baseURL?: string) {
+    if (baseURL) this.baseURL = baseURL;
+    this.request.onprogress = this.onprogress;
+    this.request.onload = this.onload;
+    this.request.onerror = this.onerror;
+    this.request.onabort = this.onabort;
+  }
 
-export default request;
+  //Methods
+  async get<T>(
+    route?: string,
+    param?: { [key: string]: any },
+    responseType?: XMLHttpRequestResponseType
+  ) {
+    const GET = "GET" as const;
+    const endURL = this.baseURL + (route ? route : "") + (param ? param : "");
+    this.request.open(GET, this.baseURL, true);
+    if (responseType) this.request.responseType = responseType;
+    return;
+  }
+
+  async post<T>() {
+    const POST = "POST" as const;
+  }
+
+  async put<T>() {
+    const PUT = "PUT" as const;
+  }
+
+  async delete<T>() {
+    const DELETE = "DELETE" as const;
+  }
+}
+
+export default Request;
