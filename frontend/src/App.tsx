fbafd65dedd908c118de846a4b30155ab009a5a9
Cj-K-7 <kllt7059@gmail.com>
2022-11-30 22:21:26 +0900

    [FRONTEND] SCROLL BOUNCE in JS

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,6 @@ import Router from "./Router/Router";
 
 function App() {
   useEffect(() => {
-    console.log("App start!");
     // fetch("http://localhost:5000/api/listAll");
   }, []);
   return <Router />;
--- frontend/src/Components/Main/Main.tsx
+++ frontend/src/Components/Main/Main.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import Modal from "../Base/Modal";
 import { classCombine } from "../../Util/cssClass";
 import "./main.css";
@@ -8,13 +8,46 @@ interface IMain extends React.HTMLAttributes<HTMLDivElement> {}
 const Main: React.FC<IMain> = ({ ...props }) => {
   const className = classCombine("main", props.className);
   const ref = useRef<HTMLDialogElement>(null);
+  const bounceLimit = 40;
+  let timeout: NodeJS.Timeout;
+
   const onClick = () => {
     ref.current?.showModal();
     console.log("??");
   };
 
+  const onScroll = (event: React.UIEvent<HTMLDivElement, UIEvent>) => {
+    const { currentTarget } = event;
+    const { scrollTop, scrollHeight, offsetHeight } = currentTarget;
+
+    console.log(scrollTop, offsetHeight, scrollHeight - bounceLimit);
+
+    clearTimeout(timeout);
+    if (scrollTop < bounceLimit) {
+      console.log("start");
+      timeout = setTimeout(() => {
+        currentTarget.scrollTo({ top: bounceLimit, behavior: "smooth" });
+      }, 369);
+    }
+    if (scrollTop + offsetHeight > scrollHeight - bounceLimit) {
+      console.log("end");
+      timeout = setTimeout(() => {
+        currentTarget.scrollTo({
+          top: scrollTop - bounceLimit,
+          behavior: "smooth",
+        });
+      }, 369);
+    }
+  };
+
   return (
-    <div className={className} onClick={onClick}>
+    <div className={className}>
+      <div className="scroll" onScroll={onScroll}>
+        <div id="a"></div>
+      </div>
+      <button type="button" onClick={onClick}>
+        modal
+      </button>
       <Modal ref={ref} type="Confirm" covered />
     </div>
   );
