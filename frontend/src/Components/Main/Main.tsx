4854070aa857635cd18b4d8480003ab2ca019de6
Cj-K-7 <kllt7059@gmail.com>
2022-12-05 22:13:18 +0900

    [FRONTEND] Touch Scroll Bouncing

--- frontend/src/Components/Main/Main.tsx
+++ frontend/src/Components/Main/Main.tsx
@@ -3,7 +3,7 @@ import Modal from "../Base/Modal";
 import { classCombine } from "../../Util/cssClass";
 import "./main.css";
 
-const bounceLimit = 40 as const;
+const bounceLimit = 200 as const;
 const transtionDuration = 276 as const;
 
 interface IMain extends React.HTMLAttributes<HTMLDivElement> {}
@@ -11,15 +11,20 @@ interface IMain extends React.HTMLAttributes<HTMLDivElement> {}
 const Main: React.FC<IMain> = ({ ...props }) => {
   const className = classCombine("main", props.className);
   const ref = useRef<HTMLDialogElement>(null);
-  const [isTouching, setIsTouching] = useState(false);
   const [startTouchY, setStartTouchY] = useState(0);
   const [endTouchY, setEndTouchY] = useState(0);
+
   const overScrollY = useMemo(() => {
     const distance = Math.abs(endTouchY - startTouchY);
-    return distance;
-  }, [startTouchY, endTouchY]);
-  let timeout: NodeJS.Timeout;
+    switch (true) {
+      case distance > bounceLimit:
+        return bounceLimit;
+      default:
+        return distance;
+    }
+  }, [endTouchY]);
 
+  console.log(overScrollY);
   const onClick = () => {
     ref.current?.showModal();
   };
@@ -27,9 +32,7 @@ const Main: React.FC<IMain> = ({ ...props }) => {
   const onTouchStart = (event: React.TouchEvent<HTMLDivElement>) => {
     const { touches } = event;
     const touch = touches[0];
-    setIsTouching(true);
-    console.log("start");
-    setEndTouchY(0);
+    setEndTouchY(touch.screenY);
     setStartTouchY(touch.screenY);
   };
 
@@ -37,27 +40,27 @@ const Main: React.FC<IMain> = ({ ...props }) => {
     const { currentTarget, touches } = event;
     const { scrollTop, scrollHeight, clientHeight } = currentTarget;
     const touch = touches[0];
-    setEndTouchY(touch.screenY);
-    if (scrollTop === 0) {
-      currentTarget.style.paddingTop = `${overScrollY}px`;
-    } else if (scrollTop + clientHeight === scrollHeight) {
-      currentTarget.style.paddingBottom = `${overScrollY}px`;
+    switch (true) {
+      case scrollTop === 0: {
+        setEndTouchY(touch.screenY);
+        currentTarget.style.paddingTop = `${overScrollY}px`;
+        break;
+      }
+      case scrollTop >= scrollHeight - clientHeight - 1: {
+        setEndTouchY(touch.screenY);
+        currentTarget.style.paddingBottom = `${overScrollY}px`;
+        break;
+      }
     }
   };
 
   const onTouchEnd = (event: React.TouchEvent<HTMLDivElement>) => {
     const { currentTarget } = event;
-    const { scrollTop, scrollHeight, clientHeight } = currentTarget;
-    console.log("end");
     currentTarget.style.transitionDuration = `${transtionDuration}ms`;
-    if (scrollTop === 0) {
-      currentTarget.style.paddingTop = `0px`;
-    } else if (scrollTop + clientHeight >= scrollHeight) {
-      currentTarget.style.paddingBottom = "0px";
-    }
+    currentTarget.style.paddingTop = `0px`;
+    currentTarget.style.paddingBottom = "0px";
     setTimeout(() => {
       currentTarget.style.transitionDuration = "0ms";
-      setIsTouching(false);
     }, transtionDuration);
   };
 
